1590579db8a697ef6922a122eefcbd302ea23be2
chenishi <chen.ishi@gmail.com>
2020-07-08 22:59:08 +0800

    Add AVX2 support on dequantization

--- global.h
+++ global.h
@@ -27,10 +27,10 @@ inline static void quantizeAVX2(char* data_ptr, uint32_t size) {
     __m256 factor_in_avx = _mm256_broadcast_ss(factor_ptr);
     // __m256 factor_in_avx = _mm256_set1_ps(1000000.0);
 
-    for (uint32_t i = 0; i < size / sizeof(__m256i); i++) {
+    for (uint32_t i = 0; i < size / sizeof(__m256); i++) {
         __m256 scaled_input_avx = _mm256_mul_ps(input_avx[i], factor_in_avx);
         __m256i output_avx = _mm256_cvtps_epi32(scaled_input_avx);
-        _mm256_packs_epi32(output_avx, _mm256_setzero_si256());
+        // _mm256_packs_epi32(output_avx, _mm256_setzero_si256());
 
        int *output = (int *)&output_avx;
        int_data_ptr[i*8] = output[0];
@@ -59,9 +59,28 @@ inline static void dequantizeNaive(char* data_ptr, uint32_t size) {
 // boost with avx 256 instructions
 inline static void dequantizeAVX2(char* data_ptr, uint32_t size) {
     float factor = 1000000.0;
+    const float *factor_ptr;
+    factor_ptr = &factor;
     int* int_data_ptr = (int*) data_ptr;
     float* float_data_ptr = (float*) data_ptr;
-    for (uint32_t i = 0; i < size; i++) {
-        float_data_ptr[i] = (float)(int_data_ptr[i] / factor);
+
+    __m256i* input_avx = (__m256i*) data_ptr;
+    __m256 factor_in_avx = _mm256_broadcast_ss(factor_ptr);
+
+
+    for (uint32_t i = 0; i < size / sizeof(__m256i); i++) {
+        __m256 scaled_output_avx = _mm256_cvtepi32_ps(input_avx[i]);
+        __m256 output_avx = _mm256_div_ps(scaled_output_avx, factor_in_avx);
+        // float_data_ptr[i] = (float)(int_data_ptr[i] / factor);
+
+        float *output = (float *)&output_avx;
+        float_data_ptr[i*8] = output[0];
+        float_data_ptr[i*8+1] = output[1];
+        float_data_ptr[i*8+2] = output[2];
+        float_data_ptr[i*8+3] = output[3];
+        float_data_ptr[i*8+4] = output[4];
+        float_data_ptr[i*8+5] = output[5];
+        float_data_ptr[i*8+6] = output[6];
+        float_data_ptr[i*8+7] = output[7];
     }
 }
